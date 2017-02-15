63e8b27ae7caa98ea26d20f63f99df09d83d8ee8
Michael Woolnough <michael.woolnough@gmail.com>
2017-02-15 18:29:16 +0000

    start of rewrite

--- music.go
+++ music.go
@@ -7,24 +7,31 @@ var (
 	Terminate  = portaudio.Terminate
 )
 
-type Wave interface {
-	next() float64
+type sounds []sound
+
+func (s sounds) Len() int {
+	return len(*s)
 }
 
-type note struct {
-	Wave
+func (s sounds) Less(i, j int) bool {
+	return s[i].Start < s[j].Start
+}
+
+func (s sounds) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type sound struct {
+	Note
 	Profile
-}
-
-func (n *note) next() (float64, bool) {
-	p, d := n.Profile.next()
-	return n.Wave.next() * p, d
+	Start, End uint32
 }
 
 type Player struct {
 	*portaudio.Stream
 	sampleRate float64
-	notes      []note
+	time       uint32
+	sounds
 }
 
 func New(sampleRate float64) (*Player, error) {
@@ -37,46 +44,9 @@ func New(sampleRate float64) (*Player, error) {
 	return p, nil
 }
 
-func (p *Player) AddSine(n Note, pr Profile) {
-	p.notes = append(p.notes, note{
-		NewSine(n, p.sampleRate),
-		pr,
-	})
-}
-
-func (p *Player) AddSaw(n Note, pr Profile) {
-	p.notes = append(p.notes, note{
-		NewSaw(n, p.sampleRate),
-		pr,
-	})
-}
-
-func (p *Player) AddTriangle(n Note, pr Profile) {
-	p.notes = append(p.notes, note{
-		NewTriangle(n, p.sampleRate),
-		pr,
-	})
-}
-
-func (p *Player) AddSquare(n Note, pr Profile) {
-	p.notes = append(p.notes, note{
-		NewSquare(n, p.sampleRate),
-		pr,
-	})
-}
-
 func (p *Player) process(data [][]float32) {
 	for i := range data[0] {
 		data[0][i] = 0
-		l := float64(len(p.notes))
-		for j := 0; j < len(p.notes); j++ {
-			s, d := p.notes[j].next()
-			data[0][i] += float32(s / l)
-			if d {
-				p.notes[j] = p.notes[len(p.notes)-1]
-				p.notes = p.notes[:len(p.notes)-1]
-				j--
-			}
-		}
+		p.time++
 	}
 }
