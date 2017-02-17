f9c4c8c88859208947e8200c0fa4ad8d74451241
Michael Woolnough <michael.woolnough@gmail.com>
2017-02-17 09:00:22 +0000

    changed floats to ints

--- music.go
+++ music.go
@@ -27,23 +27,23 @@ func (s sounds) Swap(i, j int) {
 
 type sound struct {
 	Note       Note
-	Wave       func(float64, float64) float64
-	Profile    func(float64) float64
-	Start, End uint32
+	Wave       func(int64, int64) int64
+	Profile    func(int64, int64) int64
+	Start, End int64
 }
 
-func (s sound) Val(rate, time float64) float32 {
+func (s sound) Val(rate, time uint64) uint64 {
 	return s.Profile(s.Wave(math.Mod(time * float64(s.Note) / rate)))
 }
 
 type Player struct {
 	*portaudio.Stream
-	sampleRate float64
-	time       float64
+	sampleRate int64
+	time       int64
 	sounds
 }
 
-func New(sampleRate float64) (*Player, error) {
+func New(sampleRate int64) (*Player, error) {
 	p := &Player{sampleRate: sampleRate}
 	var err error
 	p.Stream, err = portaudio.OpenDefaultStream(0, 1, sampleRate, 0, p.process)
@@ -53,9 +53,9 @@ func New(sampleRate float64) (*Player, error) {
 	return p, nil
 }
 
-func (p *Player) process(data [][]float32) {
+func (p *Player) process(data [][]int32) {
 	for i := range data[0] {
-		var f, num float32
+		var f, num int64
 		for _, sound := range p.sounds {
 			if sound.Start <= p.time && sound.End > p.time {
 				f += p.sounds[j].Val(p.sampleRate, p.time)
@@ -63,7 +63,7 @@ func (p *Player) process(data [][]float32) {
 			}
 		}
 		if num > 0 {
-			data[0][i] = f / num
+			data[0][i] = int32(f / num)
 		} else {
 			data[0][i] = 0
 		}
