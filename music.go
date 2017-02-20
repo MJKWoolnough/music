a7bac1ab3b58d8bc0e4d13793151bf54778ad902
Michael Woolnough <michael.woolnough@gmail.com>
2017-02-20 09:17:39 +0000

    added Profile type

--- music.go
+++ music.go
@@ -29,7 +29,7 @@ func (s sounds) Swap(i, j int) {
 type sound struct {
 	Note       Note
 	Wave       func(phase float64) float64
-	Profile    func(position float64) float64
+	Profile    Profile
 	channel    int
 	Start, End uint64
 }
--- profiles.go
+++ profiles.go
@@ -1,12 +1,20 @@
 package music
 
+type Profile func(pos float64) float64
+
 func Steady(_ float64) float64 {
 	return 1
 }
 
-func Adjust(amount float64) func(float64) float64 {
+func Adjust(amount float64) Profile {
 	return func(_ float64) float64 {
 		return amount
+	}
+}
+
+func AdjustProfile(amount float64, p Profile) Profile {
+	return func(pos float64) float64 {
+		return amount * p(pos)
 	}
 }
 
