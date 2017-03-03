7ad5327dc11697999546bde8d42b603a5c6ff3e2
Michael Woolnough <michael.woolnough@gmail.com>
2017-03-03 17:01:07 +0000

    slightly improved bass drum sound

--- drum/drum.go
+++ drum/drum.go
@@ -11,5 +11,10 @@ func New(p *music.Player) Drum {
 }
 
 func (d Drum) Bass(time uint64, channel int) {
-	d.player.Add(time, d.player.Rate()*2, music.E2, music.Sine, music.ExponentialDown, channel)
+	rate := d.player.Rate()
+	bassIntro := d.player.EndZeroNote(rate/50, music.E2)
+	d.player.Add(time, d.player.EndZeroNote(rate/50, music.E3), music.E3.Sine, music.RampUp, channel)
+	d.player.Add(time, d.player.EndZeroNote(rate/75, music.E4), music.E4.Sine, music.AdjustProfile(0.75, music.RampUp), channel)
+	d.player.Add(time, d.player.EndZeroNote(rate/100, music.E5), music.E5.Sine, music.AdjustProfile(0.5, music.RampUp), channel)
+	d.player.Add(time+bassIntro, rate*2, music.E2.Sine, music.ExponentialDown, channel)
 }
